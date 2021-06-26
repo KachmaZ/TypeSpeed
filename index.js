e61a76d171fdd1c69ddca154987d04619ef14825
KachmaZ <boss.kachmaz@mail.ru>
2021-06-26 21:20:59 +0300

    Добавлена вспомогательная клавиатура

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
 async function startGame(){
-    startButton.prop("disabled", true);
+    $('.start').prop("disabled", true);
     await generate();
-    let letters = $(".letter"),
-        current = 0,
-        taps = 0,
-        timer = 0,
-        speed = 0,
-        accuracy = '-';
+    letters = $(".letter"),
     setInterval(() => {
         timer++;
         speed = current / timer * 60;
+    $('.type-speed').html(speed.toFixed());
     }, 1000)
     letters[current].classList.add('current');
-    $(document).unbind('keydown');
-    $(document).on("keydown", (event) => {
-        if (event.key !== 'Shift') {
-            if(event.key === letters[current].textContent){
-                letters[current].classList.remove('current', 'mistake');
-                letters[current].classList.add('passed');
-                current++;
-                letters[current].classList.add('current');
-            }
-            else {
-                letters[current].classList.add('mistake');
-            }
-            taps++;
-            accuracy = current / taps * 100;
-            $('.type-accuracy').html(accuracy.toFixed(2));
-            $('.type-speed').html(speed.toFixed());
-        }
-    });
-
+    keyHighlighting(letters[current].textContent)
 }
 
-let startButton = $(".start");
-startButton.click(startGame);
+function keyHighlighting(currentKey){
+    console.log('"' + currentKey + '"');
+    if (currentKey === ' ') {
+        currentKey = 'Space';
+        console.log('Space')
+    }
+
+    for (let i = 0; i < supKeys.length; i++){        
+        if(supKeys[i].textContent === currentKey.toLowerCase()){
+            supKeys[i].classList.toggle('highlighted');
+            break;
+        }
+    }
+}
+
+let taps = 0,
+    letters = '',
+    current = 0,
+    timer = 0,
+    speed = 0,
+    accuracy = '-';
+
+let supKeys = $('.sup-key');
+
+$(document).on('keydown', (event) => {
+    if (event.key !== 'Shift') {
+        if(event.key === letters[current].textContent){
+            letters[current].classList.remove('current', 'mistake');
+            letters[current].classList.add('passed');
+            keyHighlighting(letters[current].textContent);
+            current++;            
+            keyHighlighting(letters[current].textContent);
+            letters[current].classList.add('current');
+        }
+        else {
+            letters[current].classList.add('mistake');
+        }
+        taps++;
+        accuracy = current / taps * 100;
+        $('.type-accuracy').html(accuracy.toFixed(2));
+    }
+});
+
+
+$(".start").click(startGame);
