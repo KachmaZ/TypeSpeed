9eb20738fbf4072a0713aced4ccf4f952c71e6af
KachmaZ <59313334+KachmaZ@users.noreply.github.com>
2021-07-01 22:32:00 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -86,31 +86,7 @@ async function start(){
 
 }
 
-
-// // Функционал вспомогательной клавиатуры
-// let supKeys = $('.sup-key');
-
-// // Подсветка клавиш
-// function keyHighlighting(currentKey){
-//     if (currentKey === ' ') {
-//         currentKey = 'Space';
-//     }
-//     for (let i = 0; i < supKeys.length; i++){        
-//         if(supKeys[i].textContent === currentKey.toLowerCase()){
-//             supKeys[i].classList.toggle('highlighted');
-//             break;
-//         }
-//     }
-// }
-
-// // Очищение подсветки
-// function clearHighlighting(){
-//     for (let i = 0; i < supKeys.length; i++){        
-//         supKeys[i].classList.remove('highlighted');
-//     }
-// }
-
-//
+// Привязка основной функции к кнопке "Старт"
 $('.start-button').click(() => {
     $('.start-title').html('Restart');
     start();
